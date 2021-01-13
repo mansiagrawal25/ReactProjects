10c5aa420ed2e442b3b610015e47e16c2cb97bd7
Mansi Agrawal <agrawal.ma@husky.neu.edu>
2021-01-13 17:01:44 -0500

    BirthdayReminderApp

---
+++ BirthdayReminderApp/build/precache-manifest.11c1f1b8af8195e6c2c28415bb513652.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92ad0742da46a2e19e42f37440375bf8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f02dfcddcd47437b094",
+    "url": "/static/css/main.5d960d03.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f02dfcddcd47437b094",
+    "url": "/static/js/main.3155035a.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
