80c88e4006b22cbff9db8facb03dffc0af636016
Mansi Agrawal <agrawal.ma@husky.neu.edu>
2021-01-16 20:44:48 -0500

    StripeMenu

---
+++ StripesMenu/src/Submenu.js
@@ -0,0 +1,48 @@
+import React, { useState, useRef, useEffect } from 'react'
+import { useGlobalContext } from './context'
+
+const Submenu = () => {
+  const {
+    isSubMenuOpen,
+    location,
+    page: { page, links },
+  } = useGlobalContext()
+
+  const conatiner = useRef(null)
+  const [columns, setColumns] = useState('col-2')
+
+  useEffect(() => {
+    setColumns('col-2')
+    const subMenu = conatiner.current
+    const { center, bottom } = location
+    subMenu.style.left = `${center}px`
+    subMenu.style.top = `${bottom}px`
+    if (links.length === 3) {
+      setColumns('col-3')
+    }
+    if (links.length > 3) {
+      setColumns('col-4')
+    }
+  }, [location, links])
+  return (
+    <aside
+      className={`${isSubMenuOpen ? 'submenu show' : 'submenu'}`}
+      ref={conatiner}
+    >
+      <h4>{page}</h4>
+      <div className={`submenu-center ${columns}`}>
+        {links.map((link, index) => {
+          const { label, icon, url } = link
+          return (
+            <a key={index} href={url}>
+              {icon}
+              {label}
+            </a>
+          )
+        })}
+      </div>
+    </aside>
+  )
+}
+
+export default Submenu
