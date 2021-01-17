3b6e0261909f9411016f4646359cddaf239fe843
Mansi Agrawal <agrawal.ma@husky.neu.edu>
2021-01-16 23:20:52 -0500

    SideBar/ModalExample

---
+++ CocktailApp/src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus
+        distinctio numquam rerum laboriosam in debitis, consequuntur natus
+        dignissimos explicabo illo id laudantium? Maiores temporibus dolorem,
+        unde iure numquam quas quo aspernatur deserunt minima optio voluptates
+        tempora nisi reprehenderit odit culpa!
+      </p>
+    </section>
+  )
+}
+
+export default About
